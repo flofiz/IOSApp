93a7f0ab6caea30e216a2d35d4378a96e2f36fb2
flofiz <flofiz@live.fr>
2020-01-10 15:40:25 +0100

    commit test

---
+++ ProjetIOS/BlockstackFunction.swift
@@ -0,0 +1,9 @@
+//
+//  BlockstackFunction.swift
+//  ProjetIOS
+//
+//  Created by Florian Fizaine on 10/01/2020.
+//  Copyright © 2020 Florian Fizaine. All rights reserved.
+//
+
+import Foundation
---
+++ ProjetIOS/ViewController2.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController2.swift
+//  ProjetIOS
+//
+//  Created by Florian Fizaine on 10/01/2020.
+//  Copyright © 2020 Florian Fizaine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
