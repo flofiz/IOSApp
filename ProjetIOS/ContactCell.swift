df4598e0a9670bc1744f58df58797ccdec73ca87
flofiz <flofiz@live.fr>
2020-01-10 17:17:48 +0100

    fin seance 3

---
+++ ProjetIOS/ContactCell.swift
@@ -0,0 +1,27 @@
+//
+//  ContactCell.swift
+//  ProjetIOS
+//
+//  Created by Florian Fizaine on 10/01/2020.
+//  Copyright © 2020 Florian Fizaine. All rights reserved.
+//
+
+import UIKit
+
+class ContactCell: UITableViewCell {
+
+    @IBOutlet weak var contactName: UILabel!
+    @IBOutlet weak var contactPhoto: UIImageView!
+    @IBOutlet weak var lastMessage: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- ProjetIOS/ViewController.swift
+++ ProjetIOS/ViewController.swift
@@ -9,16 +9,37 @@
 import UIKit
 import Blockstack
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var contactTableView: UITableView!
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contact.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "contactCell", for: indexPath) as! ContactCell
+        
+        let row = indexPath.row
+        let actual = contact[row]
+        // Configure the cell’s contents.
+        cell.contactName.text = actual.name
+        cell.contactPhoto.image = UIImage(named: "user.png")
+        cell.lastMessage.text = "last message"
+            
+        return cell
+    }
+    
 
     var contact: [Contact]  = [Contact(name: "flo dev", id: "mrfarhenheit.id.blockstack"), Contact(name: "ian", id: "ian.id.blockstack"), Contact(name: "dorine", id: "dorine.id.blockstack")]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        contactTableView.dataSource = self
         self.updateUI()
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        <#code#>
+        
     }
     
     private func updateUI(){
